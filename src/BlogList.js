9b95bd72dc29dd816715fdf80e0de7490b0771bb
llcain <llcain2012@gmail.com>
2023-06-23 15:13:35 -0500

    Set up handle delete on blog list

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,7 @@
-const BlogList = (props) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
 
-    const blogs = props.blogs;
-    const title = props.title;
+    // const blogs = props.blogs;
+    // const title = props.title;
 
     
 
@@ -12,7 +12,9 @@ const BlogList = (props) => {
                 <div className="blog-preview"key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by: { blog.author }</p>
+                    <button className="button" onClick={() => handleDelete(blog.id)}>Delete blog</button>
                 </div>
+                
             ))}
         </div>
      );
