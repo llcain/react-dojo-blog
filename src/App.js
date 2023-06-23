912d4606152f0688ee525e9ce851867b866fc955
llcain <llcain2012@gmail.com>
2023-06-22 21:07:06 -0500

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 function App() {
   const title = "Welcome to the new blog";
+  const likes = 60;
 
   return (
     <div className="App">
       <div class="content">
         <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav class="navbar">
+            <h1>The Dojo Blog</h1>
+            <div class="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
