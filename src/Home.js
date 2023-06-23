25cbc3a6c27fbebbb6d587683d4d3c85d4cadf35
llcain <llcain2012@gmail.com>
2023-06-23 10:40:49 -0500

    Created notes.txt file

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,16 @@
 const Home = () => {
 
+    let name = 'Bob';
+
     const handleClick = () => {
         console.log('hello, ninjas');
+        console.log(name);
     }
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <p>{ name }</p>
             <button onClick={handleClick}>Click me</button>
         </div>
      );
