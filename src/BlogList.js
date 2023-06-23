b0e2f8849fc8d0b9237dd926c87fdb0ef7306aee
llcain <llcain2012@gmail.com>
2023-06-23 12:47:30 -0500

    Created Blog list component and added state

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,13 @@
-const BlogList = () => {
+const BlogList = (props) => {
+
+    const blogs = props.blogs;
+    const title = props.title;
+
+    
+
     return ( 
-        <div class="blog-list">
+        <div className="blog-list">
+            <h2>{ title }</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview"key={blog.id}>
                     <h2>{blog.title}</h2>
