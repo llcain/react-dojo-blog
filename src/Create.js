85af0954aad7f591aa3857b1e8656b00c049c119
llcain <llcain2012@gmail.com>
2023-06-25 19:49:45 -0500

    Added css

--- src/Create.js
+++ src/Create.js
@@ -2,11 +2,24 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input 
+                type="text"
+                />
+                <label>Blog body:</label>
+                <textarea
+                required
+                ></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
  
 export default Create;
-<div className="create">
-    <h2>Add a New Blog</h2>
-</div>
